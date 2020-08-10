c9da7fe722421c5fd4a14300f43ff4022e9a38b7
Ben Overmyer <ben@overmyer.net>
2020-08-10 14:50:14 -0500

    Improve build script

--- build.sh
+++ build.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-pandoc -s -f markdown -t pdf -o vox-draconis-ksf.pdf front.md introduction.md gameplay.md magic.md character-creation.md character-advancement.md classes.md ancestries.md attunements.md equipment.md enemies-and-allies.md world-of-yrda.md
+DOC_TYPE=$1
+if [[ $DOC_TYPE == "latex" ]]
+then
+    pandoc -s --template=voxdraconis.latex --toc -f markdown -t latex -o vox-draconis-ksf.latex \
+        ./manuscript/introduction.md \
+        ./manuscript/gameplay.md \
+        ./manuscript/magic.md \
+        ./manuscript/character-creation.md \
+        ./manuscript/character-advancement.md \
+        ./manuscript/classes.md \
+        ./manuscript/ancestries.md \
+        ./manuscript/attunements.md \
+        ./manuscript/equipment.md \
+        ./manuscript/enemies-and-allies.md \
+        ./manuscript/world-of-yrda.md
+else
+    pandoc -s --template=voxdraconis.latex --toc -f markdown -o vox-draconis-ksf.pdf \
+        ./manuscript/introduction.md \
+        ./manuscript/gameplay.md \
+        ./manuscript/magic.md \
+        ./manuscript/character-creation.md \
+        ./manuscript/character-advancement.md \
+        ./manuscript/classes.md \
+        ./manuscript/ancestries.md \
+        ./manuscript/attunements.md \
+        ./manuscript/equipment.md \
+        ./manuscript/enemies-and-allies.md \
+        ./manuscript/world-of-yrda.md
+fi
