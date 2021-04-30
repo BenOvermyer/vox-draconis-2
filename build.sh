5fbaa8237d1fa40cbcc7bb467b1e3cb870565155
Ben Overmyer <ben@overmyer.net>
2021-04-30 07:51:35 -0500

    Use xelatex for compiling and switch typeface

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f {voxdraconis.aux|voxdraconis.out|voxdraconis.pdf|voxdraconis.ind|voxdraconis.ilg|voxdraconis.log|voxdraconis.idx}
-pdflatex voxdraconis.tex
+xelatex voxdraconis.tex
